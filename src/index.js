4f4b4c69393090cfdded08be18c51b1972a72b11
ecro <ecro98.98@gmail.com>
2022-09-08 11:53:00 -0500

    Creación de Servidor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const morgan = require('morgan');
-
+const exphbs= require('express-handlebars');
+const path= require('path');
 
 //Inicialisaciones
 
@@ -9,18 +10,40 @@ const app = express();
 
 //Settings
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+
+}));
+app.set('view engine', '.hbs');
 
 
 //Middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 
 // Global Variables
+app.use((req, res, next) => {
+
+    next();
+});
+
+
 
 // Routes
 app.use(require('./routes/'));
+app.use(require('./routes/authentication'));
+app.use('/links', require('./routes/links'));
+
 
 //Public
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 //Starting the server
