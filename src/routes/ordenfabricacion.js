ec24b363407407adb07734405349344c5ef64b75
ecro <ecro98.98@gmail.com>
2023-07-21 09:56:25 -0500

    fix: modal que se muestra a usuario de cerrar orden y agregar peso en kg delete: codigo comentado que creo yo no está haciendo nada "ordenfabricacion.js"

--- src/routes/ordenfabricacion.js
+++ src/routes/ordenfabricacion.js
@@ -4,13 +4,7 @@ const router = express.Router();
 const pool = require('../database');
 const {isLoggedIn, permissions, operador, digitador} = require('../lib/auth');
 const {ca} = require("timeago.js/lib/lang");
-//const constants = require("constants");
-//const {el} = require("timeago.js/lib/lang");
-//const {NEWDATE} = require("mysql/lib/protocol/constants/types");
-//const {logger} = require("browser-sync/dist/logger");
-//const {es, el} = require("timeago.js/lib/lang");
-//const Console = require("console");
-//const {response} = require("express");
+
 
 //Rutas de admin
 
@@ -51,49 +45,12 @@ router.post('/agregarof', isLoggedIn, operador, async (req, res) => {
     }
     const id = await pool.query('SELECT idOrdenFabricacion FROM ordenFabricacion where idUser=? and date_format(create_at, "%Y-%m-%d")=curdate() ORDER BY idOrdenFabricacion DESC LIMIT 1;', [req.user.iduser]);
     const idOrden = id[0].idOrdenFabricacion;
-    //console.log(idOrden);
     await pool.query("SET time_zone = '-05:00'");
     await pool.query('insert into operador (idtipoOperador, idUsuario, idOrdenFabricacion, idTipoMarca) values ( ?, ?, ?, ?);', [1, req.user.iduser, idOrden, 1]);
-    //console.log(idOrden)
     res.redirect('/detallesofoperador/'+idOrden)
 
 });
 
-/*
-router.post('/agregarof', isLoggedIn, operador, async (req, res) => {
-    try {
-        const { idMaquinaria, idMaterial } = req.body;
-        const fecha = new Date();
-        const hora = fecha.getHours();
-        const idTurno = (hora >= 7 && hora < 19) ? 1 : 2;
-
-        const ordenFabricacion = {
-            idMaquinaria,
-            idMaterial,
-            idUser: req.user.iduser,
-            idTurno
-        };
-
-        await pool.query("SET time_zone = '-05:00'");
-        await pool.query('INSERT INTO ordenFabricacion SET ?', [ordenFabricacion]);
-
-        const id = await pool.query('SELECT idOrdenFabricacion FROM ordenFabricacion WHERE idUser = ? AND DATE_FORMAT(create_at, "%Y-%m-%d") = CURDATE() ORDER BY idOrdenFabricacion DESC LIMIT 1', [req.user.iduser]);
-        const idOrden = id[0].idOrdenFabricacion;
-
-        await pool.query("SET time_zone = '-05:00'");
-        await pool.query('INSERT INTO operador (idtipoOperador, idUsuario, idOrdenFabricacion, idTipoMarca) VALUES (?, ?, ?, ?)', [1, req.user.iduser, idOrden, 1]);
-
-        res.redirect('/detallesofoperador/' + idOrden);
-    } catch (error) {
-        console.error(error);
-        req.flash('error', 'Ocurrió un error al agregar la orden de fabricación');
-        res.redirect('/agregarof');
-    }
-});
-
-
-
- */
 router.get('/ordenesfabricacion', isLoggedIn, operador, async (req, res) => {
     const userId = req.user.iduser;
     const rolusuario = req.user.rolusuario;
@@ -128,16 +85,13 @@ router.get('/ordenesfabricacionTerminadas', isLoggedIn, operador, async (req, re
 });
 
 router.get('/ordenesfabricacionabiertas', isLoggedIn, operador, async (req, res) => {
-    //const userId = req.user.iduser;
     const rolusuario = req.user.rolusuario;
     const datosOfCreadas = await pool.query('select * from datosof where idstatus=1;');
     res.render('produccion/operadores/ofCreadas', {datosOfCreadas, rolusuario})
-    //console.log(rolusuario);
 });
 
 
 router.get('/detallesof/:id', isLoggedIn, digitador,  async (req, res) => {
-    //const userId = req.user.iduser;
     const ordenid = req.params.id;
     const rolusuario = req.user.rolusuario;
     const idUser= req.user.iduser;
@@ -145,7 +99,6 @@ router.get('/detallesof/:id', isLoggedIn, digitador,  async (req, res) => {
     const datosPara = await pool.query('select * from datosPara where idOrdenFabricacion=?', [ordenid]);
     const horasPara = await pool.query('select sec_to_time(sum(time_to_sec(horasPara))) as horasPara, idOrdenFabricacion from horasPara where idOrdenFabricacion=?', [ordenid])
     const ayudantesOrden = await pool.query("select * from horasOperadoresCalcular where idOrden=? and idTipoOperador=2 group by idUsuario;", [ordenid]);
-    //console.log(ayudantesOrden)
     const datosAyudantes=[];
     for (const ayudante of ayudantesOrden){
         const idUsuario= ayudante.idUsuario;
@@ -156,7 +109,6 @@ router.get('/detallesof/:id', isLoggedIn, digitador,  async (req, res) => {
         const nameMaterial= ayudante.nameMaterial;
         const nameTipoOperador= ayudante.nameTipoOperador;
         const fullname= ayudante.fullname;
-        //const TiempoTrabajado= ayudante.TiempoTrabajado;
         const nameTurno= ayudante.nameTurno;
         const resultadoSumaHoras= await pool.query('SELECT SEC_TO_TIME(SUM(TIME_TO_SEC(TiempoTrabajado))) AS Hora FROM horasOperadoresCalcular WHERE idUsuario=? AND idOrden=?', [idUsuario, idOrden]);
         const horasTotales=resultadoSumaHoras[0].Hora;
@@ -180,7 +132,6 @@ router.get('/detallesof/:id', isLoggedIn, digitador,  async (req, res) => {
     const hora1 = (HorasTrabajadasMaquina).split(":");
     if(HorasParas===null){
         var hora2 = ("00:00:00").split(":");
-        //console.log(hora2);
     }else {
         var hora2 = (HorasParas).split(":");
     }
@@ -200,12 +151,9 @@ router.get('/detallesof/:id', isLoggedIn, digitador,  async (req, res) => {
         dataOperadorAyudante,
         horasymin,
         rolusuario,
-        //HorasEntrada:HorasEntrada[0],
         datosHorasOperadores,
         datosAyudantes
 
-
-        //horasEntrada
     })
     console.log(datosAyudantes);
 });
@@ -234,17 +182,12 @@ router.post('/operadormaquina/:id', isLoggedIn, async (req, res) => {
     }
     const prueba1 = await pool.query('select idHoraEntradaOf as idPrevio from horasEntradaOF where idOrden=? and idUsuario=? ORDER BY idHoraEntradaOf DESC LIMIT 1;', [idOrdenFabricacion, userId]);
     if(prueba1.length>0){
-        //console.log("Si hay")
         var idPrevio= prueba1[0].idPrevio;
     }else {
         var idPrevio=0;
     }
-    //idPrevio= prueba1[0].idPrevio;
     const prueba2 = await pool.query('select * from horasSalidaOF where idHoraEntrada=? ;', [idPrevio]);
-    //console.log(prueba2.length);
     if (prueba1.length>=0 && prueba2.length>=0 ){
-        //console.log("Entró y salió");
-        //Procede a abrir una hora entrada
         await pool.query('insert into operador set ?;', [operador]);
         await pool.query('insert into horasEntradaOF set idUsuario=?, idTipoOperador=?, idOrden=?;', [userId, idtipoOperador, idOrdenFabricacion]);
         req.flash('success', 'Operador Asignado con exito');
@@ -268,15 +211,11 @@ router.post('/salirof/:id', isLoggedIn, async (req, res) => {
         idtipoOperador,
         idTipoMarca,
     }
-    //console.log(operador);
-    //console.log(idOrdenFabricacion);
+
     await pool.query('insert into operador set ?;', [operador]);
     const idPrevio = await pool.query('select idHoraEntradaOf from horasEntradaOF where idOrden=? and idUsuario=? order by create_at desc limit 1;', [idOrdenFabricacion.id, userId]);
     console.log(idPrevio[0].idHoraEntradaOf);
     await pool.query('insert into horasSalidaOF set idUsuario=?, idTipoOperador=?, idOrden=?,idHoraEntrada=? ;', [userId, idtipoOperador, idOrdenFabricacion.id, idPrevio[0].idHoraEntradaOf]);
-
-    //await pool.query('insert into horasSalidaOF set idUsuario=?, idTipoOperador=?, idOrden=?;', [userId, idtipoOperador, idOrdenFabricacion.id]);
-
     res.redirect('/detallesofoperador/' + operador.idOrdenFabricacion);
 });
 
@@ -294,8 +233,7 @@ router.get('/detallesofoperador/:id',  isLoggedIn, permissions,async (req, res) 
     const ayudantesOrden = await pool.query('select idUsuario, idtipoOperador, idTipoMarca, u.fullname from operador inner join usuario u on operador.idUsuario=u.iduser where idtipoOperador=2 and idOrdenFabricacion=? and idTipoMarca=1 group by idUsuario;', [ordenid]);
     const HorasOperadoresOf= await pool.query("select * from horasOperadoresCalcular where idOrden=? and idUsuario=? ;", [ordenid,userId]);
     const tiempoOperador= await pool.query("select sec_to_time(sum(time_to_sec(TiempoTrabajado))) as Hora from horasOperadoresCalcular where idUsuario=? and idOrden=?;", [userId, ordenid])
-    //console.log(tiempoOperador[0].Hora);
-    //console.log(datosof);
+
     res.render('produccion/operadores/detallesofT', {
         datosof: datosof[0],
         tipoPara,
@@ -336,7 +274,6 @@ router.post('/cerrarof/:id', isLoggedIn, async (req, res) => {
     await pool.query('insert into operador (idtipoOperador, idUsuario, idOrdenFabricacion, idTipoMarca) values ( ?, ?, ?, ?);', [idtipoOperador, userId, ordenid, idTipoMarca]);
     await pool.query('update ordenFabricacion set idstatus=2 where idOrdenFabricacion=?;', [ordenid]);
     const operadores = await pool.query('select idUsuario from operador where idOrdenFabricacion=? and idtipoOperador=2 group by idUsuario;', [ordenid]);
-    //console.log(operadores);
 
     for (let i = 0; i < operadores.length; i++) {
         const idOperador = operadores[i].idUsuario;
@@ -362,10 +299,8 @@ router.post('/cerrarof/:id', isLoggedIn, async (req, res) => {
         }
     }
 
-    //const HoraFinal= horaFinal;
     await pool.query('insert into horasEntradaOF set idUsuario=?, idTipoOperador=?, idOrden=?;', [userId, 1, ordenid]);
     const idHoraEntrada = await pool.query('select idHoraEntradaOf as id from horasEntradaOF where idOrden=? and idUsuario=? and idtipoOperador=1 order by create_at desc limit 1;', [ordenid, userId]);
-    //console.log(idHoraEntrada[0].id);
     await pool.query('insert into horasSalidaOF set idUsuario=?, idTipoOperador=?, idOrden=?,idHoraEntrada=? ;', [userId, 1, ordenid, idHoraEntrada[0].id]);
     res.redirect('/detallesofoperador/' + ordenid)
 });
@@ -387,44 +322,10 @@ router.post('/buscarData', isLoggedIn, digitador, async(req, res)=>{
         const horasParas = await pool.query('SELECT sec_to_time(sum(time_to_sec(horasPara))) as horasPara, idOrdenFabricacion FROM horasPara WHERE idOrdenFabricacion=?;', [idOrden]);
         reporteParass.push(horasParas[0]);
     }
-    //console.log(reporteParass);
 
-    //console.log(esteban.length);
     res.render('produccion/reportesfechas', {reportePrincipal, reporteAyudantes,reporteParas, reporteParass})
-    //console.log(reporteParass)
 } )
 
-/* router.post('/buscarData', isLoggedIn, digitador, async (req, res) => {
-    try {
-        const { fecha1, fecha2 } = req.body;
-        await pool.query("SET time_zone = '-05:00'");
-
-        const [
-            reportePrincipal,
-            reporteAyudantes,
-            reporteParas,
-            idParas
-        ] = await Promise.all([
-            pool.query('SELECT * FROM dataOperadores WHERE FechaCompleta BETWEEN ? AND ? GROUP BY IdOrden;', [fecha1, fecha2]),
-            pool.query('SELECT Fecha, idOrden, idUsuario, fullname, nameTipoOperador, HoraEntrada, HoraSalida, TiempoTrabajado, nameTurno, nameMaquinaria, nameMaterial, HoraCompleta FROM horasOperadoresCalcular WHERE HoraCompleta BETWEEN ? AND ? AND idTipoOperador = 2;', [fecha1, fecha2]),
-            pool.query('SELECT * FROM datosPara WHERE FechaCompleta BETWEEN ? AND ?;', [fecha1, fecha2]),
-            pool.query('SELECT idOrdenFabricacion FROM datosPara WHERE FechaCompleta BETWEEN ? AND ? GROUP BY idOrdenFabricacion;', [fecha1, fecha2])
-        ]);
-
-        const reporteParass = await Promise.all(idParas.map(async (idPara) => {
-            const { idOrdenFabricacion } = idPara;
-            const [horasParas] = await pool.query('SELECT SEC_TO_TIME(SUM(TIME_TO_SEC(horasPara))) AS horasPara, idOrdenFabricacion FROM horasPara WHERE idOrdenFabricacion = ?;', [idOrdenFabricacion]);
-            return horasParas;
-        }));
-
-        res.render('produccion/reportesfechas', { reportePrincipal, reporteAyudantes, reporteParas, reporteParass });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error en el servidor');
-    }
-});
-
- */
 
 router.post('/buscar-detalle', isLoggedIn, async (req, res)=>{
     //console.log(req.body);
@@ -441,15 +342,12 @@ router.post('/buscar-detalle', isLoggedIn, async (req, res)=>{
         res.status(500).json({error: "Error al consultar la bbdd"})
     }
 
-    //console.log(result[0]);
-    //res.redirect('/detallesofoperador/213')
 })
 
 
 
 router.post('/buscarTipoPara', isLoggedIn, async (req, res)=>{
     const {IdMaquinaria}=req.body;
-    //console.log(IdMaquinaria);
     try{
         const tipos = await pool.query('select * from paraDetallesMaquina where idMaquinaria=? group by idTipoPara;', [IdMaquinaria]);
         if (tipos){
@@ -457,7 +355,6 @@ router.post('/buscarTipoPara', isLoggedIn, async (req, res)=>{
         }else{
             res.json({tipos: "Lo siento no hay datos"})
         }
-        //console.log(tipos);
     }catch (error){
         console.log('Error al consultar la bbdd')
         res.status(500).json({error:"Error al consultar la bbdd"})
